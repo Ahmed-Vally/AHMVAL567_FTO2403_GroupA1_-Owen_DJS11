83c6fa525bf0d063c5bacf7935cff49364be4b2f
Ahmed Vally <ahmedvally711@gmail.com>
2024-08-23 15:16:19 +0200

    changed the name

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import SortDropdown from './SortDropDown';
+import SortDropdown from './SortDropDown.jsx';
 import { format, parseISO } from 'date-fns';
 
 const genreMap = {
